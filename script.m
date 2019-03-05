483ada49782f9e06f0fa35ccd73279de633e89e4
B.J. Von Kugelgen <jv333@markov.eng.cam.ac.uk>
2019-03-05 17:41:25 +0000

    added new file

---
+++ script.m
@@ -0,0 +1,11 @@
+%% This script performs SSL with cause (C) and effect (E) features
+
+function p = YgivenC(C,Y,theta)
+%This function computes p(Y=1|C,theta)
+
+end
+
+function p = EgivenCY(C,Y,E,theta)
+%This function computes p(E|C,Y,theta)
+
+end
