d062f93b5d81bdbdbc0c72f8463bfc138625cca3
Julius <25486455+Juliusvk@users.noreply.github.com>
2019-04-10 15:11:27 +0100

    Started python script for experiments.

---
+++ SSLCauseEffect.py
@@ -0,0 +1,15 @@
+"""
+This script contains code to run experiments for semi-supervised learning (SSL) with cause and effect features.
+"""
+import numpy as np
+
+def sigmoid(x):
+    """
+    computes the logistic sigmoid function of evaluated at input
+    :param x: (dx1) np.array of reals
+    :return: (dx1) np.array of probabilities
+    """
+    pass
+
+
+a = np.array([1.4, 3.17, -0.45])
